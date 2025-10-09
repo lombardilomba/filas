88996c3f3eafc9477e76a53c6d6412ae1818a33d
Luis Gustavo N Lombardi <lombardi2008@gmail.com>
2025-10-09 21:13:36 +0100

    Annotate DTO fields with English JSON properties

--- src/main/java/com/example/filas/dto/AnaliseDTO.java
+++ src/main/java/com/example/filas/dto/AnaliseDTO.java
@@ -1,5 +1,6 @@
 package com.example.filas.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +10,27 @@ import java.time.LocalDateTime;
 @Setter
 public class AnaliseDTO {
 
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("proposalId")
     private Long propostaId;
+
+    @JsonProperty("queueType")
     private String tipoFila;
+
+    @JsonProperty("entryDate")
     private LocalDateTime dataEntrada;
+
+    @JsonProperty("startDate")
     private LocalDateTime dataInicio;
+
+    @JsonProperty("assignedAnalystId")
     private String analistaAtribuidoId;
+
+    @JsonProperty("currentAnalystId")
     private String analistaAtualId;
+
+    @JsonProperty("priority")
     private String prioridade;
 }
